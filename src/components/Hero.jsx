bb9d2619576f6d24325998046b54e526d2ecc208
almen1 <aeronalmendras@gmail.com>
2025-09-26 21:31:48 +0800

    chore: update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -124,7 +124,7 @@ const Hero = () => {
   return (
     <section
       ref={sectionRef}
-      className="h-[90vh] border-b relative overflow-hidden"
+      className="h-[100vh] border-b relative overflow-hidden"
       style={{
         borderBottomColor: "var(--color-secondary)",
         opacity: 0,
@@ -154,8 +154,8 @@ const Hero = () => {
           <div className="w-[66%] flex justify-start">
             <h1
               ref={roleRef}
-              className="font-main text-8xl md:text-[150px] font-bold cursor-enlarge text-neutral-500"
-              // style={{ color: "var(--color-background)" }}
+              className="font-main text-8xl md:text-[150px] font-bold cursor-enlarge"
+              style={{ color: "var(--color-background)" }}
             >
               {roles[roleIndex]}
             </h1>
